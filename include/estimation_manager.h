f0270e86c2471364c819c85ab475ad087b700795
Matej Petrlik <matej.petrlik@fel.cvut.cz>
2022-08-16 14:20:41 +0200

    started implementing estimation_manager

---
+++ include/estimation_manager.h
@@ -0,0 +1,294 @@
+#pragma once
+#ifndef ESTIMATION_MANAGER_H
+#define ESTIMATION_MANAGER_H
+
+#include <ros/ros.h>
+#include <nodelet/nodelet.h>
+
+#include <pluginlib/class_loader.h>
+#include <nodelet/loader.h>
+
+#include <geometry_msgs/QuaternionStamped.h>
+
+#include <nav_msgs/Odometry.h>
+
+#include <mrs_msgs/UavState.h>
+#include <mrs_msgs/OdometryDiag.h>
+#include <mrs_msgs/Float64Stamped.h>
+
+#include <mrs_lib/param_loader.h>
+#include <mrs_lib/publisher_handler.h>
+#include <mrs_lib/service_client_handler.h>
+
+#include "estimators/state/state_estimator.h"
+#include "reference_frame_manager.h"
+
+/*//{ class StateMachine */
+class StateMachine {
+
+
+  /*//{ states */
+public:
+  typedef enum
+  {
+
+    UNINITIALIZED_STATE,
+    INITIALIZED_STATE,
+    READY_FOR_TAKEOFF_STATE,
+    TAKING_OFF_STATE,
+    FLYING_STATE,
+    HOVER_STATE,
+    LANDING_STATE,
+    LANDED_STATE,
+    ESTIMATOR_SWITCHING_STATE,
+    EMERGENCY_STATE,
+    ERROR_STATE
+
+  } SMState_t;
+
+  /*//}*/
+
+public:
+  StateMachine(){};
+
+  bool isInState(const SMState_t &state) const {
+    std::scoped_lock lock(mtx_state_);
+    return state == current_state_;
+  }
+
+  bool isInitialized() const {
+    std::scoped_lock lock(mtx_state_);
+    return current_state_ != UNINITIALIZED_STATE;
+  }
+
+  bool isInPublishableState() const {
+    std::scoped_lock lock(mtx_state_);
+    return current_state_ == READY_FOR_TAKEOFF_STATE || current_state_ == TAKING_OFF_STATE || current_state_ == HOVER_STATE || current_state_ == FLYING_STATE ||
+           current_state_ == LANDING_STATE;
+  }
+
+  SMState_t getCurrentState() const {
+    std::scoped_lock lock(mtx_state_);
+    return current_state_;
+  }
+
+  std::string getCurrentStateString() const {
+    std::scoped_lock lock(mtx_state_);
+    return sm_state_names_[current_state_];
+  }
+
+  std::string getStateAsString(const SMState_t &state) const {
+    return sm_state_names_[state];
+  }
+
+  /*//{ changeState() */
+  bool changeState(const SMState_t &target_state) {
+
+    switch (target_state) {
+
+      case UNINITIALIZED_STATE: {
+        ROS_ERROR("[%s]: transition to %s is not possible from any state", getName().c_str(), getStateAsString(UNINITIALIZED_STATE).c_str());
+        return false;
+        break;
+      }
+
+      case INITIALIZED_STATE: {
+        if (current_state_ != UNINITIALIZED_STATE) {
+          ROS_ERROR("[%s]: transition to %s is possible only from %s", getName().c_str(), getStateAsString(INITIALIZED_STATE).c_str(),
+                    getStateAsString(UNINITIALIZED_STATE).c_str());
+          return false;
+        }
+        break;
+      }
+
+      case READY_FOR_TAKEOFF_STATE: {
+        if (current_state_ != INITIALIZED_STATE && current_state_ != LANDED_STATE) {
+          ROS_ERROR("[%s]: transition to %s is possible only from %s or %s", getName().c_str(), getStateAsString(READY_FOR_TAKEOFF_STATE).c_str(),
+                    getStateAsString(INITIALIZED_STATE).c_str(), getStateAsString(LANDED_STATE).c_str());
+          return false;
+        }
+        break;
+      }
+
+      case TAKING_OFF_STATE: {
+        if (current_state_ != READY_FOR_TAKEOFF_STATE) {
+          ROS_ERROR("[%s]: transition to %s is possible only from %s", getName().c_str(), getStateAsString(TAKING_OFF_STATE).c_str(),
+                    getStateAsString(READY_FOR_TAKEOFF_STATE).c_str());
+          return false;
+        }
+        break;
+      }
+
+      case FLYING_STATE: {
+        if (current_state_ != TAKING_OFF_STATE || current_state_ != HOVER_STATE || current_state_ != ESTIMATOR_SWITCHING_STATE) {
+          ROS_ERROR("[%s]: transition to %s is possible only from %s or %s or %s", getName().c_str(), getStateAsString(FLYING_STATE).c_str(),
+                    getStateAsString(TAKING_OFF_STATE).c_str(), getStateAsString(HOVER_STATE).c_str(), getStateAsString(ESTIMATOR_SWITCHING_STATE).c_str());
+          return false;
+        }
+        break;
+      }
+
+      case HOVER_STATE: {
+        if (current_state_ != FLYING_STATE) {
+          ROS_ERROR("[%s]: transition to %s is possible only from %s", getName().c_str(), getStateAsString(HOVER_STATE).c_str(),
+                    getStateAsString(FLYING_STATE).c_str());
+          return false;
+        }
+        break;
+      }
+
+      case ESTIMATOR_SWITCHING_STATE: {
+        if (current_state_ != FLYING_STATE) {
+          ROS_ERROR("[%s]: transition to %s is possible only from %s or %s", getName().c_str(), getStateAsString(ESTIMATOR_SWITCHING_STATE).c_str(),
+                    getStateAsString(FLYING_STATE).c_str(), getStateAsString(HOVER_STATE).c_str());
+          return false;
+        }
+        break;
+      }
+
+      case LANDING_STATE: {
+        if (current_state_ != FLYING_STATE && current_state_ != HOVER_STATE) {
+          ROS_ERROR("[%s]: transition to %s is possible only from %s or %s", getName().c_str(), getStateAsString(LANDING_STATE).c_str(),
+                    getStateAsString(FLYING_STATE).c_str(), getStateAsString(HOVER_STATE).c_str());
+          return false;
+        }
+        break;
+      }
+
+      case LANDED_STATE: {
+        if (current_state_ != LANDING_STATE) {
+          ROS_ERROR("[%s]: transition to %s is possible only from %s", getName().c_str(), getStateAsString(LANDED_STATE).c_str(),
+                    getStateAsString(LANDING_STATE).c_str());
+          return false;
+        }
+        break;
+      }
+
+      case EMERGENCY_STATE: {
+        ROS_WARN("[%s]: transition to %s", getName().c_str(), getStateAsString(EMERGENCY_STATE).c_str());
+        break;
+      }
+
+      case ERROR_STATE: {
+        ROS_WARN("[%s]: transition to %s", getName().c_str(), getStateAsString(ERROR_STATE).c_str());
+        break;
+      }
+
+      default: {
+        ROS_ERROR("[%s]: rejected transition to unknown state id %d", getName().c_str(), target_state);
+        return false;
+        break;
+      }
+    }
+
+    std::scoped_lock lock(mtx_state_);
+    {
+      previous_state_ = current_state_;
+      current_state_  = target_state;
+    }
+
+    ROS_INFO("[%s]: successfully changed states %s -> %s", getName().c_str(), getStateAsString(previous_state_).c_str(),
+             getStateAsString(current_state_).c_str());
+
+    return true;
+  }
+  /*//}*/
+
+private:
+  const std::string name_ = "StateMachine";
+
+  SMState_t current_state_  = UNINITIALIZED_STATE;
+  SMState_t previous_state_ = UNINITIALIZED_STATE;
+
+  std::mutex mtx_state_;
+
+  std::string getName() const {
+    return name_;
+  }
+
+  // clang-format off
+  const std::vector<std::string> sm_state_names_ = {
+  "UNINITIALIZED_STATE",
+  "INITIALIZED_STATE",
+  "READY_FOR_TAKEOFF_STATE",
+  "TAKING_OFF_STATE",
+  "FLYING_STATE",
+  "HOVER_STATE",
+  "LANDING_STATE",
+  "LANDED_STATE",
+  "ESTIMATOR_SWITCHING_STATE",
+  "EMERGENCY_STATE",
+  "ERROR_STATE"
+  };
+  // clang-format on
+};
+/*//}*/
+
+class EstimationManager {
+
+private:
+  const std::string nodelet_name_ = "EstimationManager";
+
+  std::string version_;
+
+
+  std::unique_ptr<mrs_uav_state_estimation::ReferenceFrameManager> ref_frame_manager_;
+
+  /* std::unique_ptr<StateMachine> sm_; */
+  StateMachine sm_;
+
+  mrs_lib::PublisherHandler<mrs_msgs::OdometryDiag> ph_diagnostics_;
+  mrs_lib::PublisherHandler<mrs_msgs::UavState>     ph_uav_state_;
+  mrs_lib::PublisherHandler<nav_msgs::Odometry>     ph_odom_main_;
+  mrs_lib::PublisherHandler<nav_msgs::Odometry>     ph_odom_slow_;  // use topic throttler instead?
+  mrs_lib::PublisherHandler<nav_msgs::Odometry>     ph_odom_main_innovation_;
+
+  mrs_lib::PublisherHandler<mrs_msgs::Float64Stamped> ph_altitude_asml_;
+  mrs_lib::PublisherHandler<mrs_msgs::Float64Stamped> ph_altitude_agl_;
+  mrs_lib::PublisherHandler<mrs_msgs::Float64Stamped> ph_max_altitude_agl_;
+
+  mrs_lib::PublisherHandler<geometry_msgs::QuaternionStamped> ph_orientation_;
+
+  ros::Timer timer_publish_;
+  double     timer_rate_publish_;
+  void       timerPublish(const ros::TimerEvent &event);
+
+  ros::Timer timer_check_health_;
+  double     timer_rate_check_health_;
+  void       timerCheckHealth(const ros::TimerEvent &event);
+
+  ros::ServiceServer srvs_change_estimator_;
+  ros::ServiceServer srvs_toggle_callbacks_;
+
+  // TODO service clients
+  /* mrs_lib::ServiceClientHandler<std_srvs::Trigger> srvch_failsafe_; */
+  /* mrs_lib::ServiceClientHandler<std_srvs::Trigger> srvc_hover_; */
+  /* mrs_lib::ServiceClientHandler<mrs_msgs::ReferenceStampedSrv> srvc_reference_; */
+  /* mrs_lib::ServiceClientHandler<std_srvs::Trigger> srvc_ehover_; */
+  /* mrs_lib::ServiceClientHandler<std_srvs::SetBool> srvc_enable_callbacks_; */
+
+  // | ------------- dynamic loading of estimators ------------- |
+  std::unique_ptr<pluginlib::ClassLoader<mrs_uav_state_estimation::StateEstimator>> estimator_loader_;  // pluginlib loader of dynamically loaded estimators
+  std::vector<std::string>                                                          _estimator_names_;  // list of estimator names
+  /* std::map<std::string, EstimatorParams>                               estimator_params_;        // map between estimator names and estimator params */
+  std::vector<boost::shared_ptr<mrs_uav_state_estimation::StateEstimator>> estimator_list_;  // list of estimators, routines are callable from this
+  std::mutex                                                               mutex_estimator_list_;
+  std::vector<std::string>                                                 estimator_names_;
+  /* int                                                                      active_estimator_idx_; */
+  boost::shared_ptr<mrs_uav_state_estimation::StateEstimator> initial_estimator_;
+  boost::shared_ptr<mrs_uav_state_estimation::StateEstimator> active_estimator_;
+  std::string                                                 initial_estimator_name_ = "UNDEFINED_INITIAL_ESTIMATOR";
+
+public:
+  EstimationManager();
+
+  void onInit();
+
+  std::string getName() const;
+};
+
+// constructor
+EstimationManager::EstimationManager() {
+}
+
+#endif
---
+++ src/estimation_manager.cpp
@@ -0,0 +1,149 @@
+#define VERSION "0.6.0.0"
+#include "estimation_manager.h"
+
+/*//{ onInit() */
+void EstimationManager::onInit() {
+  ros::NodeHandle nh = nodelet::Nodelet::getMTPrivateNodeHandle();
+
+  ros::Time::waitForValid();
+
+  ROS_INFO("[%s]: initializing", getName().c_str());
+
+  /* sm_ = std::make_unique<StateMachine>(); */
+
+  mrs_lib::ParamLoader param_loader(nh, "EstimationManager");
+
+  /*//{ check version */
+  param_loader.loadParam("version", version_);
+
+  if (version_ != VERSION) {
+
+    ROS_ERROR("[%s]: the version of the binary (%s) does not match the config file (%s), please build me!", getName().c_str(), VERSION, version_.c_str());
+    ros::shutdown();
+  }
+  /*//}*/
+
+  /*//{ load estimators */
+  param_loader.loadParam("state_estimators", estimator_names_);
+
+  estimator_loader_ = std::make_unique<pluginlib::ClassLoader<mrs_uav_state_estimation::StateEstimator>>("mrs_uav_state_estimation",
+                                                                                                         "mrs_uav_state_estimation::StateEstimator");
+
+  for (int i = 0; i < int(estimator_names_.size()); i++) {
+
+    std::string estimator_name = estimator_names_[i];
+
+    // load the estimator parameters
+    std::string address;
+    param_loader.loadParam(estimator_name + "/address", address);
+
+    try {
+      ROS_INFO("[%s]: loading the estimator '%s'", getName().c_str(), address.c_str());
+      estimator_list_.push_back(estimator_loader_->createInstance(address.c_str()));
+    }
+    catch (pluginlib::CreateClassException& ex1) {
+      ROS_ERROR("[%s]: CreateClassException for the estimator '%s'", getName().c_str(), address.c_str());
+      ROS_ERROR("[%s]: Error: %s", getName().c_str(), ex1.what());
+      ros::shutdown();
+    }
+    catch (pluginlib::PluginlibException& ex) {
+      ROS_ERROR("[%s]: PluginlibException for the estimator '%s'", getName().c_str(), address.c_str());
+      ROS_ERROR("[%s]: Error: %s", getName().c_str(), ex.what());
+      ros::shutdown();
+    }
+  }
+
+  ROS_INFO("[%s]: estimators were loaded", getName().c_str());
+  /*//}*/
+
+/*//{ check whether initial estimator was loaded */
+  param_loader.loadParam("initial_estimator", initial_estimator_name_);
+  bool initial_estimator_found = false;
+  for (int i = 0; i < int(estimator_list_.size()); i++) {
+    if (estimator_list_[i]->getName() == initial_estimator_name_) {
+      initial_estimator_ = estimator_list_[i];
+      initial_estimator_found = true;
+      break;
+    }
+  }
+
+  if (!initial_estimator_found) {
+    ROS_ERROR("[%s]: initial estimator %s could not be found among loaded estimators. shutting down", getName().c_str(), initial_estimator_name_.c_str());
+    ros::shutdown();
+  }
+/*//}*/
+
+  /*//{ initialize estimators */
+  for (int i = 0; i < int(estimator_list_.size()); i++) {
+
+    try {
+      ROS_INFO("[%s]: initializing the estimator '%s'", getName().c_str(), estimator_list_[i]->getName().c_str());
+      estimator_list_[i]->initialize(nh);
+    }
+    catch (std::runtime_error& ex) {
+      ROS_ERROR("[%s]: exception caught during tracker initialization: '%s'", getName().c_str(), ex.what());
+    }
+  }
+
+  ROS_INFO("[%s]: estimators were initialized", getName().c_str());
+
+  /*//}*/
+
+  /*//{ initialize publishers */
+  ph_uav_state_ = mrs_lib::PublisherHandler<mrs_msgs::UavState>(nh, "uav_state_out", 1);
+  ph_odom_main_ = mrs_lib::PublisherHandler<nav_msgs::Odometry>(nh, "odom_main_out", 1);
+  /*//}*/
+
+  /*//{ initialize timers */
+  param_loader.loadParam("rate/publish", timer_rate_publish_);
+  timer_publish_ = nh.createTimer(ros::Rate(timer_rate_publish_), &EstimationManager::timerPublish, this);
+
+  param_loader.loadParam("rate/health", timer_rate_check_health_);
+  timer_check_health_ = nh.createTimer(ros::Rate(timer_rate_check_health_), &EstimationManager::timerCheckHealth, this);
+  /*//}*/
+
+  sm_.changeState(StateMachine::INITIALIZED_STATE);
+
+  ROS_INFO("[%s]: initialized", getName().c_str());
+}
+/*//}*/
+
+/*//{ timerPublish() */
+void EstimationManager::timerPublish(const ros::TimerEvent& event) {
+
+  if (!sm_.isInitialized()) {
+    return;
+  }
+
+  if (sm_.isInPublishableState()) {
+
+    mrs_msgs::UavState uav_state = active_estimator_->getUavState();
+
+    // TODO state health checks
+
+    ph_uav_state_.publish(uav_state);
+
+    // TODO transform velocity to body frame and publish odom
+    
+  } else {
+    ROS_WARN_THROTTLE(1.0, "[%s]: not publishing uav state in %s", getName().c_str(), sm_.getCurrentStateString().c_str());
+  }
+}
+/*//}*/
+
+/*//{ timerCheckHealth() */
+void EstimationManager::timerCheckHealth(const ros::TimerEvent& event) {
+
+  if (!sm_.isInitialized()) {
+    return;
+  }
+
+
+}
+/*//}*/
+
+/*//{ getName() */
+std::string EstimationManager::getName() const {
+  return nodelet_name_;
+}
+/*//}*/
