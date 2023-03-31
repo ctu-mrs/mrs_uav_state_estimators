bc765ba37a9f3ab9a2b4927ce271ba9351365117
Matej Petrlik <matej.petrlik@fel.cvut.cz>
2023-03-31 13:04:21 +0200

    added missing passthrough files

---
+++ include/estimators/state/passthrough.h
@@ -0,0 +1,86 @@
+#ifndef ESTIMATORS_STATE_PASSTHROUGH_H
+#define ESTIMATORS_STATE_PASSTHROUGH_H
+
+/* includes //{ */
+
+#include <ros/ros.h>
+
+#include <nav_msgs/Odometry.h>
+
+#include <mrs_lib/lkf.h>
+#include <mrs_lib/profiler.h>
+#include <mrs_lib/param_loader.h>
+#include <mrs_lib/subscribe_handler.h>
+#include <mrs_lib/publisher_handler.h>
+#include <mrs_lib/attitude_converter.h>
+#include <mrs_lib/transformer.h>
+
+#include <mrs_uav_managers/state_estimator.h>
+
+//}
+
+namespace mrs_uav_state_estimators
+{
+
+namespace passthrough
+{
+const char name[]         = "passthrough";
+const char frame_id[]     = "passthrough_origin";
+const char package_name[] = "mrs_uav_state_estimators";
+
+using namespace mrs_uav_managers::estimation_manager;
+
+class Passthrough : public mrs_uav_managers::StateEstimator {
+
+  using CommonHandlers_t = mrs_uav_managers::estimation_manager::CommonHandlers_t;
+
+private:
+  const std::string package_name_ = "mrs_uav_state_estimators";
+
+  const std::string est_lat_name_ = "lat_passthrough";
+
+  const std::string est_alt_name_ = "alt_passthrough";
+
+  const std::string est_hdg_name_ = "hdg_passthrough";
+
+  mrs_lib::SubscribeHandler<nav_msgs::Odometry> sh_passthrough_odom_;
+  double                                        _critical_timeout_passthrough_odom_;
+  std::string                                   msg_topic_;
+
+  ros::Timer                 timer_update_;
+  void                       timerUpdate(const ros::TimerEvent &event);
+  nav_msgs::OdometryConstPtr prev_msg_;
+  bool                       first_iter_ = true;
+
+  ros::Timer timer_check_health_;
+  void       timerCheckHealth(const ros::TimerEvent &event);
+
+  bool isConverged();
+
+  void waitForEstimationInitialization();
+
+public:
+  Passthrough() : StateEstimator(passthrough::name, passthrough::frame_id, passthrough::package_name) {
+  }
+
+  ~Passthrough(void) {
+  }
+
+  void initialize(ros::NodeHandle &nh, const std::shared_ptr<CommonHandlers_t> &ch) override;
+  bool start(void) override;
+  bool pause(void) override;
+  bool reset(void) override;
+
+  mrs_msgs::UavState  getUavState() const override;
+  nav_msgs::Odometry  getInnovation() const override;
+  std::vector<double> getPoseCovariance() const override;
+  std::vector<double> getTwistCovariance() const override;
+
+  bool setUavState(const mrs_msgs::UavState &uav_state) override;
+};
+
+}  // namespace passthrough
+
+}  // namespace mrs_uav_state_estimators
+
+#endif  // ESTIMATORS_STATE_PASSTHROUGH_H
---
+++ src/estimators/state/passthrough.cpp
@@ -0,0 +1,279 @@
+/* includes //{ */
+
+#include "estimators/state/passthrough.h"
+
+//}
+
+namespace mrs_uav_state_estimators
+{
+
+namespace passthrough
+{
+
+/* initialize() //{*/
+void Passthrough::initialize(ros::NodeHandle &nh, const std::shared_ptr<CommonHandlers_t> &ch) {
+
+  ch_ = ch;
+
+  ns_frame_id_ = ch_->uav_name + "/" + frame_id_;
+
+  // | --------------- param loader initialization -------------- |
+  mrs_lib::ParamLoader param_loader(nh, getPrintName());
+
+  Support::loadParamFile(ros::package::getPath(package_name_) + "/config/estimators/" + getName() + "/" + getName() + ".yaml", nh.getNamespace());
+  param_loader.setPrefix(getName() + "/");
+
+  // | --------------------- load parameters -------------------- |
+  param_loader.loadParam("max_flight_altitude_agl", max_flight_altitude_agl_);
+  param_loader.loadParam("message/topic", msg_topic_);
+  msg_topic_ = "/" + ch_->uav_name + "/" + msg_topic_;
+
+  // | ------------------ timers initialization ----------------- |
+  timer_update_       = nh.createTimer(ros::Rate(ch_->desired_uav_state_rate), &Passthrough::timerUpdate, this, false, false);  // not running after init
+  timer_check_health_ = nh.createTimer(ros::Rate(ch_->desired_uav_state_rate), &Passthrough::timerCheckHealth, this);
+
+  // | --------------- subscribers initialization --------------- |
+  mrs_lib::SubscribeHandlerOptions shopts;
+  shopts.nh                 = nh;
+  shopts.node_name          = getPrintName();
+  shopts.no_message_timeout = mrs_lib::no_timeout;
+  shopts.threadsafe         = true;
+  shopts.autostart          = true;
+  shopts.queue_size         = 10;
+  shopts.transport_hints    = ros::TransportHints().tcpNoDelay();
+
+  sh_passthrough_odom_ = mrs_lib::SubscribeHandler<nav_msgs::Odometry>(shopts, msg_topic_);
+
+  // | ---------------- publishers initialization --------------- |
+  ph_uav_state_        = mrs_lib::PublisherHandler<mrs_msgs::UavState>(nh, Support::toSnakeCase(getName()) + "/uav_state", 1);
+  ph_odom_             = mrs_lib::PublisherHandler<nav_msgs::Odometry>(nh, Support::toSnakeCase(getName()) + "/odom", 1);
+  ph_pose_covariance_  = mrs_lib::PublisherHandler<mrs_msgs::Float64ArrayStamped>(nh, Support::toSnakeCase(getName()) + "/pose_covariance", 1);
+  ph_twist_covariance_ = mrs_lib::PublisherHandler<mrs_msgs::Float64ArrayStamped>(nh, Support::toSnakeCase(getName()) + "/twist_covariance", 1);
+  ph_innovation_       = mrs_lib::PublisherHandler<nav_msgs::Odometry>(nh, Support::toSnakeCase(getName()) + "/innovation", 1);
+  ph_diagnostics_      = mrs_lib::PublisherHandler<mrs_msgs::EstimatorDiagnostics>(nh, Support::toSnakeCase(getName()) + "/diagnostics", 1);
+
+  // | ------------------ initialize published messages ------------------ |
+  uav_state_.header.frame_id = ns_frame_id_;
+  uav_state_.child_frame_id  = ch_->frames.ns_fcu;
+
+  uav_state_.estimator_horizontal = est_lat_name_;
+  uav_state_.estimator_vertical   = est_alt_name_;
+  uav_state_.estimator_heading    = est_hdg_name_;
+
+  innovation_.header.frame_id         = ns_frame_id_;
+  innovation_.child_frame_id          = ch_->frames.ns_fcu;
+  innovation_.pose.pose.orientation.w = 1.0;
+
+  // | ------------------ finish initialization ----------------- |
+
+  if (changeState(INITIALIZED_STATE)) {
+    ROS_INFO("[%s]: Estimator initialized", getPrintName().c_str());
+  } else {
+    ROS_INFO("[%s]: Estimator could not be initialized", getPrintName().c_str());
+  }
+}
+/*//}*/
+
+/*//{ start() */
+bool Passthrough::start(void) {
+
+
+  if (isInState(READY_STATE)) {
+
+    timer_update_.start();
+    changeState(STARTED_STATE);
+    return true;
+
+  } else {
+    ROS_WARN("[%s]: Estimator must be in READY_STATE to start it", getPrintName().c_str());
+    ros::Duration(1.0).sleep();
+  }
+  return false;
+
+  ROS_ERROR("[%s]: Failed to start", getPrintName().c_str());
+  return false;
+}
+/*//}*/
+
+/*//{ pause() */
+bool Passthrough::pause(void) {
+
+  if (isInState(RUNNING_STATE)) {
+    changeState(STOPPED_STATE);
+    return true;
+  }
+  return false;
+}
+/*//}*/
+
+/*//{ reset() */
+bool Passthrough::reset(void) {
+
+  if (!isInitialized()) {
+    ROS_ERROR("[%s]: Cannot reset uninitialized estimator", getPrintName().c_str());
+    return false;
+  }
+
+  changeState(STOPPED_STATE);
+
+  ROS_INFO("[%s]: Estimator reset", getPrintName().c_str());
+
+  return true;
+}
+/*//}*/
+
+/* timerUpdate() //{*/
+void Passthrough::timerUpdate(const ros::TimerEvent &event) {
+
+
+  if (!isInitialized()) {
+    return;
+  }
+
+  const ros::Time time_now = ros::Time::now();
+
+  nav_msgs::OdometryConstPtr msg = sh_passthrough_odom_.getMsg();
+
+  if (first_iter_) {
+    prev_msg_   = msg;
+    first_iter_ = false;
+  }
+
+  {
+    std::scoped_lock lock(mtx_uav_state_);
+
+    uav_state_.header.stamp = time_now;
+
+    uav_state_.pose.position    = msg->pose.pose.position;
+    uav_state_.pose.orientation = msg->pose.pose.orientation;
+
+    uav_state_.velocity.linear = Support::rotateVector(msg->twist.twist.linear, msg->pose.pose.orientation);
+    uav_state_.velocity.angular = msg->twist.twist.angular;
+  }
+
+  {
+    std::scoped_lock lock(mtx_uav_state_, mtx_odom_);
+    odom_ = Support::uavStateToOdom(uav_state_);
+  }
+
+  {
+    std::scoped_lock lock(mtx_innovation_);
+
+    innovation_.header.stamp = time_now;
+
+    innovation_.pose.pose.position.x = prev_msg_->pose.pose.position.x - msg->pose.pose.position.x;
+    innovation_.pose.pose.position.y = prev_msg_->pose.pose.position.y - msg->pose.pose.position.y;
+    innovation_.pose.pose.position.z = prev_msg_->pose.pose.position.z - msg->pose.pose.position.z;
+  }
+
+  {
+    std::scoped_lock lock(mtx_covariance_);
+
+    pose_covariance_.header.stamp  = time_now;
+    twist_covariance_.header.stamp = time_now;
+
+    const int n_states = 6;  // TODO this should be defined somewhere else
+    pose_covariance_.values.resize(n_states * n_states);
+    pose_covariance_.values.at(n_states * AXIS_X + AXIS_X) = 1e-10;
+    pose_covariance_.values.at(n_states * AXIS_Y + AXIS_Y) = 1e-10;
+    pose_covariance_.values.at(n_states * AXIS_Z + AXIS_Z) = 1e-10;
+
+    twist_covariance_.values.resize(n_states * n_states);
+    twist_covariance_.values.at(n_states * AXIS_X + AXIS_X) = 1e-10;
+    twist_covariance_.values.at(n_states * AXIS_Y + AXIS_Y) = 1e-10;
+    twist_covariance_.values.at(n_states * AXIS_Z + AXIS_Z) = 1e-10;
+  }
+
+  publishUavState();
+  publishOdom();
+  publishCovariance();
+  publishInnovation();
+  publishDiagnostics();
+
+  prev_msg_ = msg;
+}
+/*//}*/
+
+/*//{ timerCheckHealth() */
+void Passthrough::timerCheckHealth(const ros::TimerEvent &event) {
+
+  if (!isInitialized()) {
+    return;
+  }
+
+  if (isInState(INITIALIZED_STATE)) {
+
+    if (sh_passthrough_odom_.hasMsg()) {
+      changeState(READY_STATE);
+      ROS_INFO_THROTTLE(1.0, "[%s]: Estimator is ready to start", getPrintName().c_str());
+    } else {
+      ROS_INFO_THROTTLE(1.0, "[%s]: Waiting for msg on topic %s", getPrintName().c_str(), sh_passthrough_odom_.topicName().c_str());
+      return;
+    }
+  }
+
+
+  if (isInState(STARTED_STATE)) {
+
+    changeState(RUNNING_STATE);
+  }
+}
+/*//}*/
+
+/*//{ isConverged() */
+bool Passthrough::isConverged() {
+
+  // TODO: check convergence by rate of change of determinant
+  // most likely not used in top-level estimator
+
+  return true;
+}
+/*//}*/
+
+/*//{ getUavState() */
+mrs_msgs::UavState Passthrough::getUavState() const {
+  std::scoped_lock lock(mtx_uav_state_);
+  return uav_state_;
+}
+/*//}*/
+
+/*//{ getInnovation() */
+nav_msgs::Odometry Passthrough::getInnovation() const {
+  std::scoped_lock lock(mtx_innovation_);
+  return innovation_;
+}
+/*//}*/
+
+/*//{ getPoseCovariance() */
+std::vector<double> Passthrough::getPoseCovariance() const {
+  std::scoped_lock lock(mtx_covariance_);
+  return pose_covariance_.values;
+}
+/*//}*/
+
+/*//{ getTwistCovariance() */
+std::vector<double> Passthrough::getTwistCovariance() const {
+  std::scoped_lock lock(mtx_covariance_);
+  return twist_covariance_.values;
+}
+/*//}*/
+
+/*//{ setUavState() */
+bool Passthrough::setUavState(const mrs_msgs::UavState &uav_state) {
+
+  if (!isInState(STOPPED_STATE)) {
+    ROS_WARN("[%s]: Estimator state can be set only in the STOPPED state", getPrintName().c_str());
+    return false;
+  }
+
+  ROS_WARN("[%s]: Setting the state of this estimator is not implemented.", getPrintName().c_str());
+  return false;
+}
+/*//}*/
+
+}  // namespace passthrough
+
+}  // namespace mrs_uav_state_estimators
+
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(mrs_uav_state_estimators::passthrough::Passthrough, mrs_uav_managers::StateEstimator)
