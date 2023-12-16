0a092aabf9ce806d3553616faff7e9b7c0db6f62
Tomas Baca <klaxalk@gmail.com>
2023-12-16 19:14:36 +0100

    refactored tests agains new mrs_uav_testing api

---
+++ test/gps_baro/gps_baro_basic/test.cpp
@@ -0,0 +1,67 @@
+#include <gtest/gtest.h>
+
+#include <mrs_uav_testing/test_generic.h>
+
+class Tester : public mrs_uav_testing::TestGeneric {
+
+public:
+  bool test();
+};
+
+bool Tester::test() {
+
+  {
+    auto [success, message] = activateMidAir();
+
+    if (!success) {
+      ROS_ERROR("[%s]: midair activation failed with message: '%s'", ros::this_node::getName().c_str(), message.c_str());
+      return false;
+    }
+  }
+
+  if (getActiveEstimator() != "gps_baro") {
+    ROS_ERROR("[%s]: gps_baro estimator not active", ros::this_node::getName().c_str());
+    return false;
+  }
+
+  {
+    auto [success, message] = this->gotoRel(10, 2, 1.5, 1.5);
+
+    if (!success) {
+      ROS_ERROR("[%s]: goto failed with message: '%s'", ros::this_node::getName().c_str(), message.c_str());
+      return false;
+    }
+  }
+
+  this->sleep(5.0);
+
+  if (this->isFlyingNormally()) {
+    return true;
+  } else {
+    ROS_ERROR("[%s]: not flying normally", ros::this_node::getName().c_str());
+    return false;
+  }
+}
+
+
+TEST(TESTSuite, test) {
+
+  Tester tester;
+
+  bool result = tester.test();
+
+  if (result) {
+    GTEST_SUCCEED();
+  } else {
+    GTEST_FAIL();
+  }
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
+
+  ros::init(argc, argv, "test");
+
+  testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
